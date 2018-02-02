3aaa9c880da6c2dc9ee48ae04a657e9f11861168
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-02 16:12:00 +0530

    Create st.cpp

---
+++ st.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+int len;
+    string s1[20] ;
+cout << "enter the string";
+cin>>s1;
+len=strlen(str);
+    cout << "String Length = " << len;
+}
