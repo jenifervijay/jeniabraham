a0a359b0d897d4f47e20eab5431d93b7642eb216
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-02 16:16:17 +0530

    Create st2.c

---
+++ st2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+void  main()
+{
+    char str[100];
+    int i, words;
+    printf("Enter any string: ");
+    gets(str);
+    i = 0;
+    words = 1;
+    while(str[i] != '\0')
+    {
+             if(str[i]==' ' || str[i]=='\n' || str[i]=='\t')
+        {
+            words++;
+        }
+
+        i++;
+    }
+
+    printf("Total number of words = %d", words);
+    getch();
+}
