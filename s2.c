803d7f77be7817c6580cea368e1189ab5078702e
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-07 15:56:29 +0530

    Create s2.c

---
+++ s2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s1[50],s2[50],i;
+printf("\n enter the 1st string:");
+scanf("%s",s1);
+printf(" \n enter the second string");
+scanf("%s",s2);
+if(s1[i]<=s2[i])
+{
+printf("\n %s",s2);
+}
+else if(s1[i]>=s2[i])
+{
+printf("\n%s",s1);
+}
+else
+{
+printf("\n%s%s",s1,s2);
+}
+getch();
+}
