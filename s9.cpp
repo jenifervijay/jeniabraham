5d084adb423a80301af0c62a2167315db1e2c408
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-09 15:50:34 +0530

    Create s9.cpp

---
+++ s9.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\n enter the number:");
+scanf("%d",&n);
+if((n%2)==0)
+{
+printf("yes it is a power of 2");
+}
+else
+{
+printf("not a power of 2");
+}
+getch();
+}
