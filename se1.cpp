9423e310e14f0bca8bfd9d8c5d880d41b9d23889
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-09 16:32:05 +0530

    Create se1.cpp

---
+++ se1.cpp
@@ -0,0 +1,32 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n;
+cout<<"enter the number:";
+cin>>n;
+switch(n)
+{
+case 1:cout<<"one";
+break;
+case 2:cout<<"two";
+break;
+case 3:cout<<"three";
+break;
+case 4:cout<<"four";
+break;
+case 6:cout<<"five";
+break;
+case 7:cout<<"six";
+break;
+case 8:cout<<"seven";
+break;
+case 9:cout<<"eigth";
+break;
+case 10:cout<<"nine";
+break;
+default:coutt<<"invalid";
+}
+getch();
+}
+
