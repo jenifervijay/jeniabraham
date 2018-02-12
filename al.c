0d277e721790037eeccd89e38ba9e5a1778be013
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-12 16:48:35 +0530

    Create al.c

---
+++ al.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+print("enter the string");
+scanf("%d",&n);
+if(n>='0, && n<='9' && (n>='a' && n<='z')
+{
+printf("\n string contain both alphabet and 
+
+numbers");
+}
+else
+{
+printf("\n not containing alphabet and numbers");
+}
+getch();
+}
