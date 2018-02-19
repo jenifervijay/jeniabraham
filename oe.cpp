1832006564f6fea917c26c93d7bd53c384e8a806
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-19 16:35:36 +0530

    Create oe.cpp

---
+++ oe.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+cout<<"enter a and b values:";
+cin>>a>>b;
+c=a+b;
+cot<<"\n sum=%d",c;
+if(c%2==0)
+{
+cout<<"even";
+}
+else
+{
+cout<<"odd";
+}
+getch();
+}
