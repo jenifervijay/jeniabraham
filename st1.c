72e9209e91bd283684d894d4ba401d4a4745c05e
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-19 16:02:14 +0530

    Create st1.c

---
+++ st1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[20];
+int n,i;
+clrscr();
+printf("Enter the string:");
+scanf("%s",&str);
+printf("Enter the number:");
+scanf("%d",&str);
+for(i=0;i<n;i++)
+{
+printf("%c",str[i]);
+}
+getch();
+}
