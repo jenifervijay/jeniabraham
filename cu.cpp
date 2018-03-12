533bb3b68d272a193130cedaee6e2cf9946bac4e
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-03-12 16:10:26 +0530

    Create cu.cpp

---
+++ cu.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int l,b,h,v;
+clrscr();
+cout<<"\nenter l and b and h values:";
+cin>>l>>b>>h;
+v=l*b*h;
+cout<<"volume of cuboid is:"<<v;
+getch();
+}
