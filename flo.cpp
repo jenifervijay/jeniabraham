62500bf4be12bcc83d01e5e12be03a65de11195f
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-03-12 16:00:07 +0530

    Create flo.cpp

---
+++ flo.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int p,t,r,re;
+clrscr();
+cout<<"\n enter the amount:";
+cin>>p;
+cout<<"\n enter the time:";
+cin>>t;
+cout<<"\n enter the rate:";
+cin>>r;
+re=(p*n*r)/100;
+cout<<"floor value is:"<<re;
+getch();
+}
