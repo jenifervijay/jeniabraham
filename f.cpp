31c3bb7a98f566325adc2b877b4e7f05a76507f0
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-03-12 16:37:43 +0530

    Create f.cpp

---
+++ f.cpp
@@ -0,0 +1,19 @@
+#include <iostream.h>
+#include<conio.h>
+void main()
+{
+   int n,k,i;
+      int a[20];
+clrscr();
+cout<<"\n enter n and k values:";
+cin>>n>>k;
+
+   for(i=0;i<n;i++)
+   {
+     cin>>a[i];
+   }
+  cout<<a[k-1];
+   {
+getch();
+}
+}
