2787cc3ee318134c4e67e2e21fea8e303ba0f295
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-03-16 15:52:28 +0530

    Create s.cpp

---
+++ s.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,sum=0,d,e;
+cout<<"\n enter a ,b and c value:";
+cin>>a>>b>>c;
+d=(b/a);
+e=(c/d);
+sum=d+e;
+cout<<sum;
+getch();
+}
