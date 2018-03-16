d0fdd4cfe30831c94ac31fa7a1fc9f1937ee7626
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-03-16 15:59:12 +0530

    Create fo.cpp

---
+++ fo.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n,sum=0;
+clrscr();
+cout<<"\n enter the number;";
+cin>>n;
+for(int i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+cout<<sum;
+getch();
+}
