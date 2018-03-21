d91622bd9ecedf632d1f240765aed42ffdea7e3d
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-03-21 15:57:26 +0530

    Create k.cpp

---
+++ k.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+char str[50];
+cout<<"\n enter the string";
+cin>>str;
+strcat(str,".");
+cout<<"string is:"<<str;
+getch();
+}
