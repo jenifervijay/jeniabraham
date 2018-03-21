884288f364ab563a5fab37af2fcdfeb54ebf0dcd
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-03-21 15:45:06 +0530

    Create sr.cpp

---
+++ sr.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char n[10];
+clrscr();
+cout<<"\n enter the number:";
+cin>>n;
+strrev(n);
+cout<<"reversed number:"<<n;
+getch();
+}
